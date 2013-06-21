494548a3573abff5ea74d8b4bfc8ed4f9cd92a89
Tim Abell <tim@timwise.co.uk>
2013-06-21 03:23:42 +0100

    spit it out on the console

--- Main.cs
+++ Main.cs
@@ -6,7 +6,17 @@ namespace uk.co.timwise.NumberCruncher
 	{
 		public static void Main (string[] args)
 		{
-			Console.WriteLine ("Hello World!");
+			while (true){
+				Console.Write("Enter digits: ");
+				var input = Console.ReadLine();
+				int number = 0;
+				if (!int.TryParse(input, number)) {
+					Console.WriteLine("not a valid number");
+					continue;
+				}
+				var cruncher = new NumberCruncher(number);
+				Console.WriteLine("You entered " + cruncher.ToString());
+			}
 		}
 	}
 }
